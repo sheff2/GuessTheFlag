386a844be186892374e60546e580217b5e8f845a
sheff2 <shaheff01@gmail.com>
2024-01-09 20:23:10 -0500

    Guess the flag game
    -8 round game then reset
    -guess the country out of 3 flags

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,15 +8,110 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var userScore = 0
+    @State private var rounds = 1
+    @State private var showingGameOver = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 700)
+                .ignoresSafeArea()
+            VStack{
+                Spacer()
+                
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.white)
+                
+                VStack(spacing: 15){
+                    VStack{
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .foregroundStyle(.white)
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3){ number in
+                        Button{
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(Capsule())
+                                .shadow(radius: 7)
+                        }
+                    }
+                    
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical,20)
+                .background(.regularMaterial)
+                .cornerRadius(15)
+                
+                Spacer()
+                Spacer()
+                Text(String("Score: \(userScore)"))
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                Text("Round \(rounds)")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
+        .alert(scoreTitle, isPresented: $showingScore){
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Score: \(userScore)")
+            
+        }
+        .alert("Game is Over", isPresented: $showingGameOver){
+            Button("Continue", action: gameOver)
+        } message: {
+            Text("Score: \(userScore)")
+            Text("Rounds played \(rounds)")
+            
+        }
+        
+        
     }
+    func gameOver(){
+        rounds = 0
+        userScore = 0
+        askQuestion()
+    }
+    
+    func flagTapped(_ number: Int){
+        if number == correctAnswer{
+            scoreTitle = "Correct"
+            userScore+=1
+        }
+        else{
+            scoreTitle  = "Wrong, thats the flag of \(countries[number])"
+        }
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        if(rounds < 8){
+            countries.shuffle()
+            correctAnswer = Int.random(in: 0...2)
+            rounds+=1
+        }
+        else{
+            showingGameOver = true
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
